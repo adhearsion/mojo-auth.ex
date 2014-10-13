12a772e91fd0ef7f7ff767cd9ad490f4d6569bb4
Ben Langfeld <ben@langfeld.me>
2014-10-12 23:24:40 -0300

    Very basic first doctests

--- lib/mojoauth.ex
+++ lib/mojoauth.ex
@@ -1,2 +1,20 @@
-defmodule Mojoauth do
+defmodule MojoAuth do
+  @doc ~S"""
+  Create a new set of credentials for an asserted ID, given a desired TTL and shared secret
+
+  ## Examples
+
+      iex> secret = "eN1lvHK7cXPYFNwmEwZ3QNMAiCC651E5ikuEOj7+k4EMYTXb3XxXo3iBw4ScxqzJ+aH6aDCCe++LPVGRjgfl3Q=="
+      iex> credentials = MojoAuth.create_credentials({:secret, secret})
+      iex> MojoAuth.test_credentials(credentials, secret)
+      {:ok, nil}
+
+  """
+  def create_credentials({:secret, secret}) do
+    secret
+  end
+
+  def test_credentials(credentials, secret) do
+    {:ok, nil}
+  end
 end

59b2aeb6ee7d797f1fe0001e17c14460db667a1c
Ben Langfeld <ben@langfeld.me>
2014-10-18 15:39:46 -0300

    Expire credentials after default TTL

--- lib/mojoauth.ex
+++ lib/mojoauth.ex
@@ -2,6 +2,7 @@ defmodule MojoAuth do
   use Timex
 
   @username_separator ":"
+  @default_ttl 86400
 
   @moduledoc ~S"""
   Create and verify MojoAuth credentials
@@ -19,6 +20,12 @@ defmodule MojoAuth do
       {:invalid}
 
       iex> secret = "eN1lvHK7cXPYFNwmEwZ3QNMAiCC651E5ikuEOj7+k4EMYTXb3XxXo3iBw4ScxqzJ+aH6aDCCe++LPVGRjgfl3Q=="
+      iex> credentials = MojoAuth.create_credentials(secret: secret)
+      iex> use Timex
+      iex> MojoAuth.test_credentials(credentials, secret, Date.now |> Date.universal |> Date.shift(days: 1, secs: 1) |> Date.convert(:secs)) # Pretend it's the future
+      {:expired, nil}
+
+      iex> secret = "eN1lvHK7cXPYFNwmEwZ3QNMAiCC651E5ikuEOj7+k4EMYTXb3XxXo3iBw4ScxqzJ+aH6aDCCe++LPVGRjgfl3Q=="
       iex> credentials = MojoAuth.create_credentials(id: "foobar", secret: secret)
       iex> MojoAuth.test_credentials(credentials, secret)
       {:ok, "foobar"}
@@ -27,26 +34,34 @@ defmodule MojoAuth do
       iex> credentials = MojoAuth.create_credentials(id: "doodah", secret: secret)
       iex> MojoAuth.test_credentials([username: 'foobar', password: credentials[:password]], secret)
       {:invalid}
+
+      iex> secret = "eN1lvHK7cXPYFNwmEwZ3QNMAiCC651E5ikuEOj7+k4EMYTXb3XxXo3iBw4ScxqzJ+aH6aDCCe++LPVGRjgfl3Q=="
+      iex> credentials = MojoAuth.create_credentials(id: "foobar", secret: secret)
+      iex> use Timex
+      iex> MojoAuth.test_credentials(credentials, secret, Date.now |> Date.universal |> Date.shift(days: 1, secs: 1) |> Date.convert(:secs)) # Pretend it's the future
+      {:expired, "foobar"}
   """
 
   @doc "Create a new set of credentials for an asserted ID, given a desired TTL and shared secret"
   def create_credentials(id: id, secret: secret) do
-    username = Enum.join(["foobar", id], @username_separator)
+    expiry_timestamp = now |> Date.shift(secs: @default_ttl) |> Date.convert(:secs)
+    username = Enum.join([expiry_timestamp, id], @username_separator)
     [username: username, password: sign(username, secret)]
   end
   def create_credentials(secret: secret) do
     create_credentials(id: nil, secret: secret)
   end
 
-  def test_credentials(credentials, secret) do
+  @doc "Test that credentials are valid and current"
+  def test_credentials(credentials, secret, timestamp \\ now |> Date.convert(:secs)) do
     password = credentials[:password]
     case sign(credentials[:username], secret) do
       ^password ->
         case String.split(credentials[:username], @username_separator, trim: true) do
           [expiry, id] ->
-            {:ok, id}
+            {status(expiry, timestamp), id}
           [expiry] ->
-            {:ok, nil}
+            {status(expiry, timestamp), nil}
         end
       _ ->
         {:invalid}
@@ -55,5 +70,16 @@ defmodule MojoAuth do
 
   defp sign(message, secret) do
     :crypto.hmac(:sha, secret, message) |> :base64.encode
+  end
+
+  defp now do
+    Date.now |> Date.universal
+  end
+
+  defp status(expiry, timestamp) do
+    case String.to_integer(expiry) > timestamp do
+      true -> :ok
+      false -> :expired
+    end
   end
 end
--- mix.exs
+++ mix.exs
@@ -25,6 +25,6 @@ defmodule Mojoauth.Mixfile do
   #
   # Type `mix help deps` for more examples and options
   defp deps do
-    []
+    [{:timex, "~> 0.12.9"}]
   end
 end
